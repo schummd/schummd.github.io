22232b39db87022a82cb330e5138991ed6da11ab
Daria Schumm <zonamoroza@gmail.com>
2024-10-18 15:57:59 +0200

    theses page

--- src/pages/Publications.jsx
+++ src/pages/Publications.jsx
@@ -7,52 +7,52 @@ export default function Publications() {
         <ul class="font-montserrat space-y-10 list-none list-inside">
           <li>
             D. Schumm, K. O. E. Müller and B.Stiller,
-            <a href="#" class="font-semibold text-indigo-500">“Are We There Yet? A Study of Decentralised Identity Applications”</a>,
+            <a href="#" class="font-semibold text-indigo-500"> "Are We There Yet? A Study of Decentralised Identity Applications"</a>,
             2025 (In Submission).
           </li>
           <li>
             D. Schumm, B. Rodrigues and B. Stiller,
-            <a href="#" class="font-semibold text-indigo-500">“Digital Identity and Blockchain: A Comprehensive Overview of Approaches”</a>,
+            <a href="#" class="font-semibold text-indigo-500"> "Digital Identity and Blockchain: A Comprehensive Overview of Approaches"</a>,
             in Blockchain and Society Handbook, John Flood and Lachlan Robb,
             De Gruyter, 2025 (Forthcoming).
           </li>
           <li>
             B. Byers, P. Emmenegger, J. Hunhevicz, D. Schumm, F. Heisel, D. Hall and C. De Wolf,
-            <a href="http://dx.doi.org/10.2139/ssrn.4946912" class="font-semibold text-indigo-500 cursor-pointer" target="_blank">Decentralized Phygital Identifiers for Circular Construction</a>
+            <a href="http://dx.doi.org/10.2139/ssrn.4946912" class="font-semibold text-indigo-500 cursor-pointer" target="_blank"> "Decentralized Phygital Identifiers for Circular Construction"</a>
           </li>
           <li>
             K. O. E. Müller, D. Datsomor, D. Schumm, B. Rodrigues and B. Stiller,
-            <a href="#" class="font-semibold text-indigo-500">“Big Brother is Watching You: Non-Intrusive ZigBee User Profiling”</a>,
+            <a href="#" class="font-semibold text-indigo-500"> "Big Brother is Watching You: Non-Intrusive ZigBee User Profiling"</a>,
             2024 20th International Conference on Network and Service Management (CNSM),
             Prague, Czech Republic, 2024.
           </li>
           <li>
             M. Leder, D. Schumm, B. Rodrigues and B. Stiller,
-            <a href="#" class="font-semibold text-indigo-500">“AfriBit: A People-Oriented Wallet Fostering Financial Inclusion in Kibera”</a>,
+            <a href="#" class="font-semibold text-indigo-500"> "AfriBit: A People-Oriented Wallet Fostering Financial Inclusion in Kibera"</a>,
             6th Conference on Blockchain Research & Applications for Innovative Networks and Services (BRAINS),
             Berlin, Germany, 2024.
           </li>
           <li>
             A. R. Veres, D. Schumm, T. Grübl, K. O. E. Müller, B. Rodrigues and B. Stiller,
-            <a href="https://ieeexplore.ieee.org/document/10639716" class="font-semibold text-indigo-500 cursor-pointer" target="_blank">“Demo: Secure Inventorying and Lifecycle Management of IoT with DLT”</a>,
+            <a href="https://ieeexplore.ieee.org/document/10639716" class="font-semibold text-indigo-500 cursor-pointer" target="_blank"> "Demo: Secure Inventorying and Lifecycle Management of IoT with DLT"</a>,
             IEEE 49th Conference on Local Computer Networks (LCN),
             Caen, France, 2024.
           </li>
           <li>
             K. O. E. Müller, D. Schumm, E. Wallace, B. Rodrigues and B. Stiller,
-            <a href="https://ieeexplore.ieee.org/document/10575751/" class="font-semibold text-indigo-500 cursor-pointer" target="_blank">"SHIFT: a Security and Home Integration Framework for IoT"</a>,
+            <a href="https://ieeexplore.ieee.org/document/10575751/" class="font-semibold text-indigo-500 cursor-pointer" target="_blank"> "SHIFT: a Security and Home Integration Framework for IoT"</a>,
             NOMS 2024-2024 IEEE Network Operations and Management Symposium,
             Seoul, Korea, Republic of, 2024, pp. 1-7.
           </li>
           <li>
             D. Schumm, R. Mukta and H. -y. Paik,
-            <a href="https://ieeexplore.ieee.org/document/10237019" class="font-semibold text-indigo-500 cursor-pointer" target="_blank">"Efficient Credential Revocation Using Cryptographic Accumulators"</a>,
+            <a href="https://ieeexplore.ieee.org/document/10237019" class="font-semibold text-indigo-500 cursor-pointer" target="_blank"> "Efficient Credential Revocation Using Cryptographic Accumulators"</a>,
             2023 IEEE International Conference on Decentralized Applications and Infrastructures (DAPPS),
             Athens, Greece, 2023, pp. 127-134.
           </li>
           <li>
             D. Schumm, R. Mukta and H. -y. Paik,
-            <a href="https://ieeexplore.ieee.org/document/10174975" class="font-semibold text-indigo-500 cursor-pointer" target="_blank">"Efficient Credential Revocation Using Cryptographic Accumulators"</a>,
+            <a href="https://ieeexplore.ieee.org/document/10174975" class="font-semibold text-indigo-500 cursor-pointer" target="_blank"> "Efficient Credential Revocation Using Cryptographic Accumulators"</a>,
             2023 IEEE International Conference on Blockchain and Cryptocurrency (ICBC),
             Dubai, United Arab Emirates, 2023, pp. 1-3.
           </li>
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -20,8 +20,8 @@ export default function Works() {
               <div>
                 <a href="https://iteratio.netlify.app" class="font-semibold text-indigo-500" target="_blank">
                   Iteratio
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 inline-block ml-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-4 inline-block ml-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
                   </svg>
                 </a>
               </div>
@@ -43,8 +43,8 @@ export default function Works() {
               <div>
                 <a href="https://dariaschummphoto.netlify.app" class="font-semibold text-indigo-500" target="_blank">
                   Schumm Photography
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 inline-block ml-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-4 inline-block ml-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
                   </svg>
                 </a>
               </div>
