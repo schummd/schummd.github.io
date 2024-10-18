1094327722d2e9fb6317cfdc196529c4eeb7ff71
Daria Schumm <zonamoroza@gmail.com>
2024-10-18 15:54:32 +0200

    theses page

---
+++ src/pages/Theses.jsx
@@ -0,0 +1,61 @@
+export default function Theses () {
+  return(
+    <div class="flex flex-col items-center justify-center h-full">
+      <div class="container w-4/5 md:w-3/5 my-28">
+        <h2 class="font-montserrat font-medium text-3xl my-14">Supervised Theses</h2>
+
+        <div class="grid md:gap-x-6 gap-y-1 font-montserrat">
+
+          <div class="col-span-2 md:col-span-1 mb-2">2024</div>
+          <div class="col-span-2 md:col-span-1">
+            <div class="grid col-span-2 md:col-span-1 gap-1">
+              <div>
+                <span class="font-semibold text-indigo-500">Cedric Egon von Rauscher: </span>
+                Design and Implementation of Privacy-Preserving Mechanisms for Metadata and Public Keys in Blockchain-Based Self-Sovereign Identities
+              </div>
+            </div>
+            <div class="text-slate-500">Bachelor degree</div>
+          </div>
+          <div class="col-span-2 border-b border-stone-300 my-4"/>
+
+          <div class="col-span-2 md:col-span-1 mb-2">2024</div>
+          <div class="col-span-2 md:col-span-1">
+            <div class="grid col-span-2 md:col-span-1 gap-1">
+              <div>
+                <span class="font-semibold text-indigo-500">Simona Borghi: </span>
+                Design and Prototype of a Blockchain-Based Credential Revocation for IoT Device Lifecycle Management
+              </div>
+            </div>
+            <div class="text-slate-500">Bachelor degree</div>
+          </div>
+          <div class="col-span-2 border-b border-stone-300 my-4"/>
+
+          <div class="col-span-2 md:col-span-1 mb-2">2024</div>
+          <div class="col-span-2 md:col-span-1">
+            <div class="grid col-span-2 md:col-span-1 gap-1">
+              <div>
+                <span class="font-semibold text-indigo-500">Justin Verhoek: </span>
+                Design and Evaluation of Biometric Data Management in Blockchain-Based Self-Sovereign Identities
+              </div>
+            </div>
+            <div class="text-slate-500">Bachelor degree</div>
+          </div>
+          <div class="col-span-2 border-b border-stone-300 my-4"/>
+
+          <div class="col-span-2 md:col-span-1 mb-2">2024</div>
+          <div class="col-span-2 md:col-span-1">
+            <div class="grid col-span-2 md:col-span-1 gap-1">
+              <div>
+                <span class="font-semibold text-indigo-500">Pascal Emmenegger: </span>
+                Design, Implementation, and Analysis of Decentralized Product Passport Systems for Circular Construction.
+              </div>
+            </div>
+            <div class="text-slate-500">Master degree, collaboration with ETH</div>
+          </div>
+          <div class="hidden col-span-2 border-b border-stone-300 my-4"/>
+
+        </div>
+      </div>
+    </div>
+  )
+}
